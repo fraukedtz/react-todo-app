2fe941dd603d700cbc94baca487488e0ddad1cca
Frauke <frauke@MacBook-Pro-84.local>
2018-11-09 11:03:00 +0100

    add todos array, .map logic and doneToggle function

--- src/App.js
+++ src/App.js
@@ -5,8 +5,35 @@ import Input from './Input.js'
 import Todo from './Todo.js'
 
 class App extends Component {
+  state = {
+    todos: []
+  }
+
   log = text => {
     console.log(text)
+  }
+
+  renderTodos() {
+    return this.state.todos.map((item, index) => (
+      <Todo
+        key={item.text}
+        text={item.text}
+        onClick={() => this.toggleDone(index)}
+      />
+    ))
+  }
+
+  toggleDone = index => {
+    const { todos } = this.state
+    const newTodos = [
+      ...todos.slice(0, index),
+      { ...todos[index], isDone: !todos[index].isDone },
+      ...todos.slice(index + 1)
+    ]
+
+    this.setState({
+      todos: newTodos
+    })
   }
 
   render() {
@@ -14,10 +41,7 @@ class App extends Component {
       <section className="App">
         <Heading />
         <Input />
-        <ul>
-          {/* {this.renderDoors()} */}
-          <Todo text="shopping" onClick={() => this.log('shopping')} />
-        </ul>
+        <ul>{this.renderTodos()}</ul>
       </section>
     )
   }
