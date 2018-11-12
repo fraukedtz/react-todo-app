27ae3ad2526eb0d992f6e02b69f86221ae2b41f7
Frauke <frauke@MacBook-Pro-84.local>
2018-11-12 14:12:40 +0100

    add docs folder

---
+++ docs/precache-manifest.c0c0dec770eb4852aacbba2f660ea342.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "13ba84e8a1da945ee9cb",
+    "url": "/static/js/main.13ba84e8.chunk.js"
+  },
+  {
+    "revision": "fa92568d88c7bfad485c",
+    "url": "/static/js/1.fa92568d.chunk.js"
+  },
+  {
+    "revision": "13ba84e8a1da945ee9cb",
+    "url": "/static/css/main.9c338a24.chunk.css"
+  },
+  {
+    "revision": "2dbebad215509c25b2badd16b44c6120",
+    "url": "/index.html"
+  }
+];
